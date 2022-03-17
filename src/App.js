a18ef40ed08a69fabac49c04a3b2172e0ea34900
Nayan1002 <nayansorathiya1002@gmail.com>
2022-03-17 10:07:16 +0530

    web ui done

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from 'react';
 import Navbarmain from './Navbarmain';
 import Body from './Body';
 import Footer from './Footer';
+// import animation from './assets/images/CK_Minter_BGAnimation.mp4';
 
 function App() {
   return (
     <>
+      {/* <div className='page'> */}
       <div className='background'>
+        {/* <video  autoPlay="autoplay" loop="loop" id="videoloop" src={animation}>
+      </video> */}
         <div className='overlay'>
           <Navbarmain />
           <Body />
           <Footer />
+          <div className='w-50'>
+
+          </div>
         </div>
       </div>
+      {/* </div> */}
+
     </>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -4,13 +4,27 @@ import polylogo from './assets/images/polygon_logo.png';
 
 function Footer() {
     return (<>
-        <div className="container pb-4">
+        {/* <div className="container pb-4">
             <div className="d-flex justify-content-center align-items-center">
                 <div className="d-flex justify-content-between w-75">
                     <div><img src={drivelogo} alt="drive logo" width="70%"></img></div>
                     <div className="d-flex align-items-center"><span className="poweredby">powered by :</span><img src={polylogo} alt="plogo" width="50%"></img></div>
                 </div>
             </div>
+        </div> */}
+        <div className="footer py-4 d-flex  flex-lg-column">
+
+            <div className=" container w-75 d-flex flex-column flex-md-row justify-content-between align-items-center ">
+
+                <div className="nav nav-dark order-1 order-md-1 justify-content-center align-items-center">
+                    <img src={drivelogo} alt="drive logo" width="70%"></img>
+                </div>
+                <div className="nav nav-dark order-2 order-md-2 justify-content-center align-items-center pt-3">
+                    <span className="poweredby">powered by :</span><img src={polylogo} alt="plogo" width="50%"></img>
+                </div>
+
+            </div>
+
         </div>
     </>);
 }
